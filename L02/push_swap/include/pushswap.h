5bcf816368e8d322883fcd703861fbbff9daa8ea
minoka kakizaki <minoka.kakizaki@gmail.com>
2024-06-13 00:40:37 +0900

    clean up and going to bed

--- L02/push_swap/include/pushswap.h
+++ L02/push_swap/include/pushswap.h
@@ -6,7 +6,7 @@
 /*   By: mkakizak <mkakizak@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/02 15:55:50 by mkakizak          #+#    #+#             */
-/*   Updated: 2024/06/12 23:51:03 by mkakizak         ###   ########.fr       */
+/*   Updated: 2024/06/13 00:37:17 by mkakizak         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -21,6 +21,11 @@ typedef struct c_list
 	int 			head;
 }	t_c_list;
 
+// # define true = 1
+// # define false = 0
+
+// extern char *ARGS = "1 2 3 4 5 6";
+
 t_c_list	*ft_c_lstnew(int content);
 int			ft_c_lstsize(t_c_list **lst);
 void		ft_c_lstadd(t_c_list **head, t_c_list *new);
@@ -31,6 +36,9 @@ t_c_list 	*ft_lstpop(t_c_list **head);
 void		ft_lstswap(t_c_list **head);
 void		ft_c_lstiter(t_c_list *head, int (*f)(int));
 int 		add2(int number);
+
+int args_validation(int argc, char *argv[]);
+
 
 
 
