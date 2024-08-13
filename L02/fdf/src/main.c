02a46c83158d16d3decb3678f256125f527fd3e0
minokakakizaki <minoka.kakizaki@gmail.com>
2024-08-13 15:53:37 +0900

    pusing

--- L02/fdf/src/main.c
+++ L02/fdf/src/main.c
@@ -12,6 +12,14 @@
 
 #include <fdf.h>
 
+void my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char *dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+
 int	main(int argc, char *argv[])
 {
 	// int		fd;
@@ -27,6 +35,11 @@ int	main(int argc, char *argv[])
 	mlx = mlx_init();
 	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
 	img.img =  mlx_new_image(mlx, 1920, 1080);
+
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+
+	mlx_pixel_put(&img, mlx_win, 5, 5, 0x00FF0000);
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 
 	mlx_loop(mlx);
 
