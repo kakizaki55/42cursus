546a835c5ff3d94e9d5a4ca34fbf74aa752b3b61
minokakakizaki <minoka.kakizaki@gmail.com>
2024-08-27 13:34:44 +0900

    a fiannly have a matrix and it seems to be working great

--- L02/fdf/src/parse_map.c
+++ L02/fdf/src/parse_map.c
@@ -12,20 +12,22 @@
 
 #include <fdf.h>
 
-int *convert_to_int(char **array, int len)
+int **convert_to_int(char **array, int len)
 {
 	int i;
 	int *res;
 
 	i = 0;
-	res = ft_calloc(sizeof(int), len);
+	res = ft_calloc(sizeof(int*), len);
+	if( res == NULL)
+		return (NULL);
 
 	while(array[i])
 	{
 		res[i] = ft_atoi(array[i]);
 		i++;
 	}
-	return (res);
+	return (free_all(array), res);
 }
 
 int **parse_map(int fd)
@@ -45,16 +47,19 @@ int **parse_map(int fd)
 		str = get_next_line(fd);
 		if (str)
 		{
-			ft_printf("%s\n", str);
+			// ft_printf("%s\n", str);
 			array = ft_split(str, ' ');
 			if(array == NULL)
 				return (NULL);
-			// // same with this 11
-			// res[i] = convert_to_int(array, 11);
+			// // same with this 19 need to be the lenght of the array.i t is now set for 42.fdf
+			res[i] = convert_to_int(array, 19);
+			// print_arr(res[i], 19);
+			// puts("\n");
 		}
 		else
 			// free_all(array);
 			bool = FALSE;
 	}
+	print_matrix(res, 11, 19);
     return (res);
 }
--- L02/fdf/src/utils.c
+++ L02/fdf/src/utils.c
@@ -1,17 +1,27 @@
 
 #include <fdf.h>
 
-// void print_img(t_data img)
-// {
-// 	ft_printf("bits per pixel is: %d\n", img.bits_per_pixel);
-// 	ft_printf("line length: %d\n", img.line_length);
-// 	ft_printf("img: %p\n", img.img);
-// 	ft_printf("addr: %p\n", img.addr);
-// }
-
-void print_matrix(int **matix)
+void print_img(t_data img)
 {
+	ft_printf("bits per pixel is: %d\n", img.bits_per_pixel);
+	ft_printf("line length: %d\n", img.line_length);
+	ft_printf("img: %p\n", img.img);
+	ft_printf("addr: %p\n", img.addr);
+}
 
+void print_matrix(int **matrix, int rows, int cols)
+{
+    int i, j;
+
+    for (i = 0; i < rows; i++) {
+        printf("[%d] ", i);
+
+        for (j = 0; j < cols; j++) {
+            printf(", %d ", matrix[i][j]);
+        }
+
+        printf("\n");
+    }
 }
 
 void	free_all(char **str)
