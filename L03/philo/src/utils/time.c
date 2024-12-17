a7a88c19e8f5a0fc5721f81743af7be11de6d803
Kakizaki55 <minoka.kakizaki@gmail.com>
2024-12-17 15:31:04 +0900

    fixed code now hhave running threads

--- L03/philo/src/utils/time.c
+++ L03/philo/src/utils/time.c
@@ -6,7 +6,7 @@
 /*   By: mkakizak <mkakizak@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 15:26:10 by mkakizak          #+#    #+#             */
-/*   Updated: 2024/12/17 15:26:23 by mkakizak         ###   ########.fr       */
+/*   Updated: 2024/12/17 15:28:12 by mkakizak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 time_t	get_time_in_ms(void)
 {
-    struct timeval	tv;
-    time_t			time_in_ms;
+	struct timeval	tv;
+	time_t			time_in_ms;
 
-    gettimeofday(&tv, NULL);
-    time_in_ms = tv.tv_sec * 1000 + tv.tv_usec / 1000;
-    return (time_in_ms);
+	gettimeofday(&tv, NULL);
+	time_in_ms = tv.tv_sec * 1000 + tv.tv_usec / 1000;
+	return (time_in_ms);
 }
