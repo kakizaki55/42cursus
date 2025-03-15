c6f1300e22bde3eeca9518c72cb50e775d849ede
minokakakizaki <minoka.kakizaki@gmail.com>
2025-03-16 03:29:18 +0900

    got a working version of the funciton

--- L04/cpp/cpp00/ex00/megaphone.cpp
+++ L04/cpp/cpp00/ex00/megaphone.cpp
@@ -1,14 +1,23 @@
-
 #include <iostream>
 #include <cstring>
+#include <string>
+
 
 int main(int argc, char **argv)
 {
+	std::string res = "";
 
 	if (argc < 2) {
-		std::cout << "* LOUD NOISE *";
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	} else {
-		std:: cout << "you have more then one argument";
+		for (int i = 1; i < argc; i++)
+		{
+			std::string temp = argv[i];
+			for (size_t j = 0; j < temp.length(); j++)
+				temp[j] = std::toupper(temp[j]);
+			res += temp;
+		}
+		std::cout << res;
 	}
 	std::cout << std::endl;
 	return 0;
