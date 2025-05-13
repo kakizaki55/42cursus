28d993283eef36d5e0b0e2d39144b5ee89272df4
Kakizaki55 <minoka.kakizaki@gmail.com>
2025-05-13 14:48:43 +0900

    clean commit

---
+++ L04/cpp/cpp01/ex04/src/main.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <fstream>
+
+int main(int argc, char** argv) {
+    if(argc != 4)
+    {
+        std::cerr << "Usage: " << argv[0] << " <file_name> <str1> <str2>" << std::endl;
+        return 1;
+    }
+
+    std::string file_name = argv[1];
+    std::string str1 = argv[2];
+    std::string str2 = argv[3];
+    std::string replace  = ".replace";
+
+    if(file_name.empty() || str1.empty() || str2.empty())
+    {
+        std::cerr << "Error: All arguments must be non-empty." << std::endl;
+        return 1;
+    }
+
+    std::ifstream input_file(file_name.c_str());
+    if (!input_file.is_open()) {
+        std::cerr << "Error opening file: " << file_name << std::endl;
+        return 1;
+    }
+    std::ofstream output_file(file_name.append(".replace").c_str());
+    if (!output_file.is_open()) {
+        std::cerr << "Error opening output file: output.txt" << std::endl;
+        if(input_file.is_open())
+            input_file.close();
+        return 1;
+    }
+
+    
+
+
+
+
+    std:: string line;
+    while(getline(input_file, line))
+    {
+        int index = 0;
+        index = line.find(str1);
+
+        if(index >= 0)
+        {
+            line.erase(index, str1.length());
+            line.insert(index, str2);
+            std::cout << "line: " << line << std::endl;
+        }
+        else
+        {
+            // printf("str1 not found in line.\n");
+        }
+        output_file << line;
+        output_file << std::endl;
+    }
+
+
+
+
+    
+    std::cout << "This is a C++ program." << std::endl;
+    return 0;
+}
+
---
+++ L04/cpp/cpp01/ex05/src/main.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <stdlib.h>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    // return 0;
+    void *str = calloc(sizeof(char), 100);
+    exit(0);
+    return 0;
+}
+
