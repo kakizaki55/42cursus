49edda190cb223861bc994d7907a50c5393b7465
minokakakizaki <minoka.kakizaki@gmail.com>
2025-06-11 21:36:36 +0900

    statrting cpp 02 :

---
+++ L04/cpp/cpp02/sandbox.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+
+int main(){
+	// Print a message to the console
+	printf("Hello, World!\n");
+
+	float x = 1.000;
+	int y = 1;
+
+	if(x == y)
+	{
+		puts("these are the same number");
+	}
+	else
+	{
+		puts("these are not the same number");
+	}
+
+	// Return from the main function
+	return(0);
+}
