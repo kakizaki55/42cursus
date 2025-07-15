0733fa19432601ed47bd3f85bbd7b32d2b2564ec
Kakizaki55 <minoka.kakizaki@gmail.com>
2025-07-15 14:50:56 +0900

    clean commit

--- L04/cpp/cpp01/ex04/src/main.cpp
+++ L04/cpp/cpp01/ex04/src/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <fstream>
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv)
+{
+
     if(argc != 4)
     {
         std::cerr << "Error: must have 3 args: " << argv[0] << " <file_name> <str1> <str2>" << std::endl;
@@ -13,42 +15,48 @@ int main(int argc, char** argv) {
     std::string str2 = argv[3];
     std::string replace  = ".replace";
 
-    if(file_name.empty() || str1.empty() || str2.empty())
+    if(file_name.empty() || str1.empty())
     {
-        std::cerr << "Error: All arguments must be non-empty." << std::endl;
+        std::cerr << "Error: you must have a file name and string to find." << std::endl;
         return 1;
     }
 
     std::ifstream input_file(file_name.c_str());
-    if (!input_file.is_open()) {
+    if (!input_file.is_open())
+    {
         std::cerr << "Error opening file: " << file_name << std::endl;
         return 1;
     }
 
     std::ofstream output_file(file_name.append(".replace").c_str());
-    if (!output_file.is_open()) {
+    if (!output_file.is_open()) 
+    {
         std::cerr << "Error opening output file: output.txt" << std::endl;
         if(input_file.is_open())
             input_file.close();
         return 1;
     }
 
-    std:: string line;
+    if (str1 == str2)
+    {
+        std::cerr << "Error: you cant replace the string with the same string"<< std:: endl;
+        return 1;
+    }
 
+    std:: string line;
     while(getline(input_file, line))
     {
         int index = 0;
-        index = line.find(str1);
-        while((index = line.find(str1)) != -1)
+        index = line.find(str1, index);
+        while(index != -1)
         {
             line.erase(index, str1.length());
             line.insert(index, str2);
-            index += line.length();
+            index += str2.length();
+            index = line.find(str1, index);
         }
-
         output_file << line << std:: endl;
     }
 
     return 0;
 }
-
