fe8297bd30da6957b1243cb90bf80d0f807ec37e
minokakakizaki <minoka.kakizaki@gmail.com>
2026-02-18 12:00:22 +0900

    done with ex00

---
+++ L05/cpp/cpp08/ex00/include/easyfind.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <algorithm>
+#include <exception>
+
+class NotFoundException : public std::exception {
+public:
+	const char* what() const throw() {
+		return "Element not found in container";
+	}
+};
+
+template <typename T>
+typename T::iterator easyfind(T& container, int value) {
+	typename T::iterator it = std::find(container.begin(), container.end(), value);
+	if (it == container.end()) {
+		throw NotFoundException();
+	}
+	return it;
+}
