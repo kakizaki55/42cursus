f688174fe4dc578f6ed96568bdda2caed4ee1c4b
Kakizaki55 <minoka.kakizaki@gmail.com>
2026-02-19 14:34:47 +0900

    all done

--- L05/cpp/cpp08/ex02/include/MutantStack.hpp
+++ L05/cpp/cpp08/ex02/include/MutantStack.hpp
@@ -6,32 +6,26 @@
 template <typename T>
 class MutantStack : public std::stack<T> {
 public:
-	// Orthodox Canonical Form
-	MutantStack() : std::stack<T>() {}
-	MutantStack(const MutantStack& other) : std::stack<T>(other) {}
-	MutantStack& operator=(const MutantStack& other) {
-		if (this != &other) {
-			std::stack<T>::operator=(other);
-		}
-		return *this;
-	}
-	~MutantStack() {}
+	MutantStack();
+	MutantStack(const MutantStack& other);
+	MutantStack& operator=(const MutantStack& other);
+	~MutantStack();
 
-	// Iterator types from the underlying container
+	// Iterator types from the underlying container (std::deque by default)
 	typedef typename std::stack<T>::container_type::iterator iterator;
 	typedef typename std::stack<T>::container_type::const_iterator const_iterator;
 	typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
 	typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
 
-	// Iterator functions
-	iterator begin() { return this->c.begin(); }
-	iterator end() { return this->c.end(); }
-	const_iterator begin() const { return this->c.begin(); }
-	const_iterator end() const { return this->c.end(); }
+	iterator begin();
+	iterator end();
+	const_iterator begin() const;
+	const_iterator end() const;
 
-	// Reverse iterator functions
-	reverse_iterator rbegin() { return this->c.rbegin(); }
-	reverse_iterator rend() { return this->c.rend(); }
-	const_reverse_iterator rbegin() const { return this->c.rbegin(); }
-	const_reverse_iterator rend() const { return this->c.rend(); }
+	reverse_iterator rbegin();
+	reverse_iterator rend();
+	const_reverse_iterator rbegin() const;
+	const_reverse_iterator rend() const;
 };
+
+#include "MutantStack.tpp"
